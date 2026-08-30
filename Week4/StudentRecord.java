4e96a9e8cbb1160f3ef52a7716b76b2523bcf1be
prateek2105-ops <prateek25csu376@ncuindia.edu>
2026-08-30 14:36:05 +0530

    Update StudentRecord.java

--- Week4/StudentRecord.java
+++ Week4/StudentRecord.java
@@ -1,1 +1,18 @@
+public class StudentRecord {
+    public static void main(String[] args) {
 
+        Student s1 = new Student("Prateek", "S101", 49);
+        Student s2 = new Student("Dev", "S102", 53);
+        Student s3 = new Student("Rahul", "S103", 78);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        s1.setMark(80);
+        System.out.println(s1);
+
+        s1.setMark(120);
+        System.out.println(s1);
+    }
+}
